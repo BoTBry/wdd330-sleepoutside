254d4e0fe0c4ea5c4f07def26add6b0fd829f83d
BoTBry <abchigbu@gmail.com>
2025-11-11 17:32:10 +0100

    week1

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,16 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Step 1: Get the current cart or create a new one
+  let currentCart = JSON.parse(localStorage.getItem("so-cart")) || [];
+
+  // Step 2: Add the new product
+  currentCart.push(product);
+
+  // Step 3: Save the updated cart
+  setLocalStorage("so-cart", currentCart);
+
+  // setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
